17a82203db11f0a2ab0e34d5607cef79e4e1d169
crostonj@gmail.com <crostonj@gmail.com>
2016-10-21 11:30:40 -0600

    Initial Checkin

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var os = require("os");
+var babyNames = require('./babyNames')();
+
+
+var port = 4001;
+
+
+app.get('/', function (req, res) {
+
+    res.send("Hello World");
+});
+
+app.get('/girls', function (req, res) {
+    var rank = req.query["rank"];
+    res.send(babyNames.getGirlName(rank));
+});
+
+app.get('/boys', function (req, res) {
+    var rank = req.query['rank'];
+    res.send(babyNames.getBoyName(rank));
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+});
---
+++ babyNames.js
@@ -0,0 +1,88 @@
+
+var babyNames = function () {
+    var names = [
+        {
+            "rank": "1",
+            "name": "Noah",
+            "sex": "boy"
+        },
+        {
+            "rank": "1",
+            "name": "Emma",
+            "sex": "girl"
+        },
+        {
+            "rank": "2",
+            "name": "Liam",
+            "sex": "boy"
+        },
+        {
+            "rank": "2",
+            "name": "Olivia",
+            "sex": "girl"
+        },
+        {
+            "rank": "3",
+            "name": "Ethan",
+            "sex": "boy"
+        },
+        {
+            "rank": "3",
+            "name": "Ava",
+            "sex": "girl"
+        },
+        {
+            "rank": "4",
+            "name": "Lucas",
+            "sex": "boy"
+        },
+        {
+            "rank": "4",
+            "name": "Sophia",
+            "sex": "girl"
+        },
+        {
+            "rank": "5",
+            "name": "Mason",
+            "sex": "boy"
+        },
+        {
+            "rank": "5",
+            "name": "Isabella",
+            "sex": "girl"
+        }
+    ];
+
+
+    function getBoyName(rank) {
+        return grep(names, function (p) { return p.sex == 'boy' && p.rank == rank; })
+            .map(function (p) { return p.name; });
+    }
+
+    function getGirlName(rank) {
+        return grep(names, function (p) { return p.sex == 'girl' && p.rank == rank; })
+            .map(function (p) { return p.name; });;
+    }
+    return {
+        getBoyName: getBoyName,
+        getGirlName: getGirlName
+    }
+};
+
+module.exports = babyNames;
+
+var grep = function (items, callback) {
+    var filtered = [],
+        len = items.length,
+        i = 0;
+    for (i; i < len; i++) {
+        var item = items[i];
+        var cond = callback(item);
+        if (cond) {
+            filtered.push(item);
+        }
+    }
+
+    return filtered;
+};
+
